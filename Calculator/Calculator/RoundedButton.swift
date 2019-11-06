155cb3cae5c4c871048397f3cb7e5e3252daff6d
BF <bf@xhs.technology>
2019-11-07 09:49:28 +1100

    RoundedButtons default their highlighted colour to white, and their title color for selected to the background color.

--- Calculator/Calculator/RoundedButton.swift
+++ Calculator/Calculator/RoundedButton.swift
@@ -16,35 +16,41 @@ class RoundedButton: UIButton {
     
     override var isHighlighted: Bool {
         didSet {
-            if self.isHighlighted {
-                self.backgroundColor = selectedColor ?? self.highlightedColor
-            } else {
-                self.backgroundColor = self.defaultBackgroundColor
-            }
+            self.updateColorsForState(state: self.state)
         }
     }
     
     override var isSelected: Bool {
         didSet {
-            if self.isSelected {
-                self.backgroundColor = selectedColor ?? self.defaultBackgroundColor
-            } else {
-                self.backgroundColor = self.defaultBackgroundColor
-            }
+            self.updateColorsForState(state: self.state)
         }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         self.layer.masksToBounds = true
+        
         self.defaultBackgroundColor = self.backgroundColor
-        self.highlightedColor = self.highlightedColor ?? self.defaultBackgroundColor
+        self.highlightedColor = self.highlightedColor ?? UIColor.white
         self.selectedColor = self.selectedColor ?? self.defaultBackgroundColor
+        
+        self.setTitleColor(self.backgroundColor, for: UIControl.State.highlighted)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.cornerRadius = min(self.frame.width, self.frame.height)/2.0
+    }
+    
+    private func updateColorsForState(state: UIControl.State) {
+        switch state {
+        case .highlighted:
+            self.backgroundColor = self.highlightedColor
+        case .selected:
+            self.backgroundColor = self.selectedColor
+        default:
+            self.backgroundColor = self.defaultBackgroundColor
+        }
     }
 
 }
