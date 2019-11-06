5a5cbd6695953e31b858620f1d0589e7a0b3b847
BF <bf@xhs.technology>
2019-11-07 09:29:22 +1100

    Buttons now provide more visual feedback when pressed.

--- Calculator/Calculator/RoundedButton.swift
+++ Calculator/Calculator/RoundedButton.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class RoundedButton: UIButton {
-
+    
+    var defaultBackgroundColor: UIColor? = nil
+    @IBInspectable var highlightedColor: UIColor? = nil
+    @IBInspectable var selectedColor: UIColor? = nil
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if self.isHighlighted {
+                self.backgroundColor = selectedColor ?? self.highlightedColor
+            } else {
+                self.backgroundColor = self.defaultBackgroundColor
+            }
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            if self.isSelected {
+                self.backgroundColor = selectedColor ?? self.defaultBackgroundColor
+            } else {
+                self.backgroundColor = self.defaultBackgroundColor
+            }
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         self.layer.masksToBounds = true
+        self.defaultBackgroundColor = self.backgroundColor
+        self.highlightedColor = self.highlightedColor ?? self.defaultBackgroundColor
+        self.selectedColor = self.selectedColor ?? self.defaultBackgroundColor
     }
     
     override func layoutSubviews() {
