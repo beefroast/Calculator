8a44bbd9bb6c538b3d0224febd30bd87296a7daf
BF <bf@xhs.technology>
2019-11-06 16:33:18 +1100

    Adds default project.

---
+++ Calculator/Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Benjamin Frost on 6/11/19.
+//  Copyright © 2019 Benjamin Frost. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
