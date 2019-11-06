16dc9d99a2115f047ef2c2f7c7babfe9f66688f0
BF <bf@xhs.technology>
2019-11-06 17:40:42 +1100

    Adds a stack for storing the state of the calculator.

--- Calculator/Calculator/CalculatorViewController.swift
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+enum Operator {
+    case plus
+    case minus
+    case multiply
+    case divide
+    case equals
+    case clear
+    case plusOrMinus
+    case percent
+}
 
+
+class CalculatorViewController: UIViewController {
+        
+    @IBOutlet weak var labelOutput: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - IBActions
+    
+    @IBAction func onNumeralPressed(_ sender: UIButton) {
+        
+        guard let numeral = sender.titleLabel?.text else { return }
+
+        
+        
+    }
+    
 
 
 }
